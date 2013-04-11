154be29d98357032107f6422df854863e61445ff
Ben Rady <benrady@gmail.com>
2013-04-10 19:13:49 -0700

    Added roost module

---
+++ roost/__init__.py
@@ -0,0 +1,3 @@
+"""
+Roost module
+"""
--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+long_description = """
+Similar to the Nest thermostat, Roost adapts to your habits and figures out an optimal schedule for heat, A/C, and humidity controls.
+
+Unlike the Nest, Roost is built around open standards and easy data access. Roost does not rely on any external services. It provides an API for data access and control and can be assembled from open-source software and hardware.
+"""
 
 setup(name='Roost',
       version='0.1',
       description='Open-Source Adaptive Thermostat',
+      long_description=long_description,
       author='Ben Rady',
       author_email='benrady@gmail.com',
+      packages=find_packages(),
+      install_requires=['XBee>=2.0.0', 'pyserial>=2.6'],
       url='github.com/benrady/roost',
-      packages=['distutils', 'distutils.command'],
-      requires=['xbee', 'pyserial'])
+      packages=['roost', 'distutils.command'],
+     )
