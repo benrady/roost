2fb40ff52b8c4a55b472e04d67104e8448f8b7ab
Ben Rady <benrady@gmail.com>
2013-04-15 21:37:09 -0500

    Making usb port configurable

--- roost/main.py
+++ roost/main.py
@@ -6,9 +6,9 @@ import sys, serial
 
 from xbee_monitor import read_loop
 
-def main():
-  print 'Starting Roost'
-  ser = serial.Serial('/dev/ttyUSB0', 9600)
+def main(device='/dev/ttyUSB0'):
+  print 'Starting Roost. Connecting to ' + device
+  ser = serial.Serial(device, 9600)
   xbee = ZigBee(ser, escaped=True)
   while True:
     try:
@@ -19,4 +19,4 @@ def main():
   sys.exit(0)
 
 if __name__ == "__main__":
-  main()
+  main(*sys.argv[1:])
