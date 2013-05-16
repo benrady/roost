7bec37a618b1280bff8d3231ad5b3321c1cc898f
Ben Rady <benrady@gmail.com>
2013-05-15 22:06:31 -0500

    Added arg parser to main.py

--- roost/main.py
+++ roost/main.py
@@ -5,17 +5,34 @@ from twisted.internet import reactor
 
 #from xbee_reader import open_port
 
+import sys, getopt
+
 class Simple(resource.Resource):
     isLeaf = True
     def render_GET(self, request):
         return "<html>Hello, world!</html>"
 
-def main(device='/dev/ttyUSB0', data_dir='/var/roost/data'):
-  root = static.File('public') # FIXME This should be a parameter
+def start_reactor(opts):
+  web_dir = opts.get('--web-dir', '/var/roost/www')
+  device = opts.get('--device', '/dev/ttyUSB0')
+  print "Starting Roost"
+  root = static.File(web_dir) 
   root.putChild('hello', Simple())
   reactor.listenTCP(8080, server.Site(root))
   #xbee_reader.open_port(device)
   reactor.run()
 
+def main(argv=None):
+  if argv is None:
+    argv = sys.argv
+  try:
+    opts, args = getopt.getopt(argv[1:], "d:w:", ["device=", "web-dir="])
+    return start_reactor(dict(opts));
+  except getopt.error, msg:
+    raise Exception(msg)
+    print >>sys.stderr, err.msg
+    print >>sys.stderr, "for help use --help"
+    return 2
+
 if __name__ == "__main__":
-  main()
+  sys.exit(main());
