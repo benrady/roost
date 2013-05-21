72101e425a88fbd8a7bbc2eb63e58c4ed56c5161
Ben Rady <benrady@gmail.com>
2013-05-20 21:37:32 -0500

    new layout

--- web/public/js/roost.js
+++ web/public/js/roost.js
@@ -4,22 +4,25 @@ function homeView() {
   function addForecast(data) {
     forecast.empty();
     _.each(data.forecast.simpleforecast.forecastday, function(day) {  
-      forecast.append($('<div>').addClass('span3').append(
-        $('<h3>').text(day.date.weekday),
+      forecast.append($('<div>').addClass('span3 small-text').append(
+        $('<h4>').text(day.date.weekday),
         $('<img>').
           attr('src', day.icon_url).
           attr('title', day.conditions).
           addClass('img-polaroid text-wrap'),
         $('<div>').append(
           $('<div>').text(day.high.fahrenheit + '° / ' + day.low.fahrenheit + '°'),
-          $('<div>').text(day.avehumidity + '% Humidity'),
+          $('<div>').text(day.avehumidity + '% H'),
           $('<div>').text(day.avewind.mph + "mph " + day.avewind.dir))));
     });
   }
 
   function fetchForecast() {
-    $.getJSON('http://api.wunderground.com/api/' + settings.wuKey + '/forecast/q/60630.json?callback=?', addForecast);
-    //$.getJSON('/data/forecast.json', addForecast);
+    if (window.location.hostname === 'localhost') {
+      $.getJSON('/data/forecast.json', addForecast);
+    } else {
+      $.getJSON('http://api.wunderground.com/api/' + settings.wuKey + '/forecast/q/60630.json?callback=?', addForecast);
+    }
   }
 
   var forecast = $('<div>').addClass('forecast-list');
--- web/public/js/settings.js
+++
@@ -1,5 +0,0 @@
-var settings = {
-  wuKey: 'b73359ae29af5669',
-  pushoverToken: 'UKDnO2TXaZPgYcChv3wPVXesTrjY2A',
-  pushoverUsers: ['yC9dY1ZCKu4geifal7lQ7Bqh1kFMGN']
-};
