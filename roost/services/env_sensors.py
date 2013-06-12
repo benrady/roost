771d477989aa0ce4cfdf5e91755d29bd91aba7e3
Ben Rady <benrady@gmail.com>
2013-06-12 16:11:06 -0500

    new format for xbee data; new init.d script

--- roost/services/env_sensors.py
+++ roost/services/env_sensors.py
@@ -6,18 +6,18 @@ class Source:
     self.name = name
     self.calibration = calibration
 
-  def read_sample(self, frame, pin):
+  def read_sample(self, samples, pin):
     # XBee analog pins
     # 0 - 1.2v = 0 - 1023
-    if frame['samples'][0].has_key(pin):
-      return 1200 * (frame['samples'][0][pin] / 1023.0)
+    if samples[0].has_key(pin):
+      return 1200 * (samples[0][pin] / 1023.0)
 
-  def parse_frame(self, frame):
+  def parse_samples(self, samples):
     reading = {'zone': self.name}
-    pin0 = self.read_sample(frame, 'adc-0')
+    pin0 = self.read_sample(samples, 'adc-0')
     if pin0:
       reading.update({'temp_f': (pin0 / 10) + self.calibration})
-    pin1 = self.read_sample(frame, 'adc-1')
+    pin1 = self.read_sample(samples, 'adc-1')
     if pin1: 
       reading.update({'humidity': (pin1 - 0.22) * 0.073632 })
     return reading
@@ -26,18 +26,20 @@ class EnvSensors(service.Service):
   def __init__(self, opts={}):
     self.setName('env_sensors')
     self.sources = { 
-        '\x00\x13\xa2\x00@\x89\xe5C': Source("zone1", -0.82),
-        '\x00\x13\xa2\x00@\x89\xe5D': Source("zone2")
+        '0:13:a2:0:40:89:e5:43': Source("zone1", -0.82),
+        '0:13:a2:0:40:89:e5:44': Source("zone2")
     }
-    self.properties = {
+    self.zones = {
         "zone1": {"name": "Zone 1"},
         "zone2": {"name": "Zone 2"}
     }
 
   def on_data(self, event, data):
-    packet = data['packet']
-    reading = self.sources[packet['source_addr_long']].parse_frame(packet)
-    self.properties[reading['zone']].update(reading)
+    reading = self.sources[data['source']].parse_samples(data['samples'])
+    self.zones[reading['zone']].update(reading)
+
+  def properties(self):
+    return {"zones": self.zones}
 
   def startService(self):
     service.Service.startService(self)
--- tests/services/test_env_sensors.py
+++ tests/services/test_env_sensors.py
@@ -9,10 +9,14 @@ class TestEnvSensors(unittest.TestCase):
     self.service = env_sensors.EnvSensors()
 
   def test_test_zones(self):
-    eq_(self.service.properties['zone1'], {"name": "Zone 1"})
+    eq_(self.service.properties()['zones'], {
+      'zone1': {"name": "Zone 1"},
+      'zone2': {"name": "Zone 2"}
+    })
 
   def test_on_data(self):
-    packet = {'source_addr_long': '\x00\x13\xa2\x00@\x89\xe5D', 'source_addr': 'r\xda', 'id': 'rx_io_data_long_addr', 'samples': [{'adc-0': 580, 'adc-1': 570}], 'options': '\x01'}
-    self.service.on_data('xbee.data', {"packet": packet})
-    eq_(self.service.properties['zone2']['temp_f'], 68.03519061583577)
-    eq_(self.service.properties['zone2']['humidity'], 49.215754039178876)
+    data = {'source': '0:13:a2:0:40:89:e5:43', 'samples':[{'adc-0': 580, 'adc-1': 570}]}
+    self.service.on_data('xbee.data', data)
+    zones = self.service.properties()['zones']
+    eq_(zones['zone1']['temp_f'], 67.21519061583578)
+    eq_(zones['zone1']['humidity'], 49.215754039178876)
