c28fe72f2f03cbc4540a9313189908f33e58992d
Ben Rady <benrady@gmail.com>
2013-06-19 22:09:38 -0500

    refactoring env_sensors

--- roost/services/env_sensors.py
+++ roost/services/env_sensors.py
@@ -5,44 +5,41 @@ import roost
 def now_millis():
   return int(round(time.time() * 1000))
 
-class Source:
-  def __init__(self, zone, calibration=0.0):
-    self.zone = zone
-    self.calibration = calibration
+class EnvSensors(service.Service):
+  def __init__(self, opts={}):
+    self.setName('env_sensors')
+    self.calibration = 0.0
+    # FIXME Need a way to assign sources to zones
+    self.zones = {
+        "zone1": {"name": "Zone 1", "source": '0:13:a2:0:40:89:e5:43'},
+        "zone2": {"name": "Zone 2", "source": '0:13:a2:0:40:89:e5:44'}
+    }
 
-  def read_sample(self, samples, pin):
+  def _read_sample(self, samples, pin):
     # XBee analog pins
     # 0 - 1.2v = 0 - 1023
     if samples[0].has_key(pin):
       return 1200 * (samples[0][pin] / 1023.0)
 
-  def parse_samples(self, samples):
+  def _parse_samples(self, samples):
     reading = {'lastUpdate': now_millis()}
-    pin0 = self.read_sample(samples, 'adc-0')
+    pin0 = self._read_sample(samples, 'adc-0')
     if pin0:
       reading.update({'tempF': (pin0 / 10) + self.calibration})
-    pin1 = self.read_sample(samples, 'adc-1')
+    pin1 = self._read_sample(samples, 'adc-1')
     if pin1: 
       reading.update({'humidity': (pin1 - 0.22) * 0.073632 })
     return reading
 
-class EnvSensors(service.Service):
-  def __init__(self, opts={}):
-    self.setName('env_sensors')
-    # FIXME Need a way to assign sources to zones
-    self.sources = { 
-        '0:13:a2:0:40:89:e5:43': Source("zone1", -2.5),
-        '0:13:a2:0:40:89:e5:44': Source("zone2")
-    }
-    self.zones = {
-        "zone1": {"name": "Zone 1"},
-        "zone2": {"name": "Zone 2"}
-    }
+  def _find_zone(self, source):
+    for k, zone in self.zones.items():
+      if zone['source'] == source: 
+        return zone
 
   def on_data(self, event, data):
-    source = self.sources[data['source']]
-    reading = source.parse_samples(data['samples'])
-    self.zones[source.zone].update(reading)
+    zone = self._find_zone(data['source'])
+    reading = self._parse_samples(data['samples'])
+    zone.update(reading)
 
   def properties(self):
     return {"zones": self.zones}
--- tests/services/test_env_sensors.py
+++ tests/services/test_env_sensors.py
@@ -11,8 +11,8 @@ class TestEnvSensors(unittest.TestCase):
 
   def test_test_zones(self):
     eq_(self.service.properties()['zones'], {
-      'zone1': {"name": "Zone 1"},
-      'zone2': {"name": "Zone 2"}
+        "zone1": {"name": "Zone 1", "source": '0:13:a2:0:40:89:e5:43'},
+        "zone2": {"name": "Zone 2", "source": '0:13:a2:0:40:89:e5:44'}
     })
 
   @patch('roost.services.env_sensors.now_millis')
@@ -20,6 +20,6 @@ class TestEnvSensors(unittest.TestCase):
     now.return_value = 1234567890000
     self.service.on_data('xbee.data', self.data)
     zones = self.service.properties()['zones']
-    eq_(zones['zone1']['tempF'], 65.53519061583577)
+    eq_(zones['zone1']['tempF'], 68.03519061583577)
     eq_(zones['zone1']['humidity'], 49.215754039178876)
     eq_(zones['zone1']['lastUpdate'], 1234567890000)
